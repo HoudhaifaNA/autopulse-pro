26b68939e4185a8d6fe84acb4b04128da1abd829
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-17 20:08:38 +0100

    Rebased first changes from main

---
+++ renderer/styles/GlobalStyle.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    
+    *, 
+    *::before,
+    *::after{
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+    html{
+        font-size: 62.25%;
+        scroll-behavior: smooth;
+    }
+   
+  
+`;
+
+export default GlobalStyle;
