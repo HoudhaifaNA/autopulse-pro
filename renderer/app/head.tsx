30c9a82d02f1c03cd07ab05dbc932f3983592dfd
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-18 15:04:53 +0100

    edit head component

--- renderer/app/head.tsx
+++ renderer/app/head.tsx
@@ -2,7 +2,7 @@ const Head = () => {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Home Page</title>
+      <title>Login Page</title>
     </>
   );
 };
