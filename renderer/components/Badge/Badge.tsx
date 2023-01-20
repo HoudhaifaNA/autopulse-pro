f7787762cb2269ae5b2e08e2dbacf88e6e880376
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-20 19:12:55 +0100

    added badge components

---
+++ renderer/components/Badge/Badge.tsx
@@ -0,0 +1,17 @@
+import { BadgeWrapper } from "./BadgeStyle";
+
+export interface BadgeProps {
+  // these names don't describe the status of the badge just for theme colors names
+  type: "success" | "warning" | "error";
+  children: string;
+}
+
+const Badge = ({ type, children }: BadgeProps) => {
+  return (
+    <BadgeWrapper type={type}>
+      <span>{children}</span>
+    </BadgeWrapper>
+  );
+};
+
+export default Badge;
---
+++ renderer/components/Badge/BadgeStyle.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import styled from "styled-components";
+import { BadgeProps } from "./Badge";
+
+type BadgeWrapperProps = Omit<BadgeProps, "children">;
+
+export const BadgeWrapper = styled.div<BadgeWrapperProps>`
+  width: fit-content;
+  padding: 0.4rem 1rem;
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-transform: capitalize;
+  background-color: ${({ theme, type }) => theme.colors[type]["200"]};
+  color: ${({ theme, type }) => theme.colors[type]["700"]};
+  border-radius: 10rem;
+`;
