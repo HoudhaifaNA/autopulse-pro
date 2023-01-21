0f352b6faaf864aeaa850de3bd3464d20293357f
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-21 09:00:40 +0100

    added basic input

--- renderer/app/page.tsx
+++ renderer/app/page.tsx
@@ -1,9 +1,12 @@
 import { Heading1 } from "styles/Typography";
+import Input from "components/Form/Input";
 
 const LoginPage = () => {
   return (
     <div style={{ padding: "4rem" }}>
       <Heading1>Login Page</Heading1>
+      <Input />
+      <Input />
     </div>
   );
 };
