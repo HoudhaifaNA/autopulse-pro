03270c4f83914565b241e147499c7b1e8b9626aa
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-26 17:00:38 +0100

    added empty state component

---
+++ renderer/components/EmptyState/EmptyState.style.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const EmptyStateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 4rem;
+`;
+
+export const EmptyStateImage = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
+export const EmptyStateText = styled.div`
+  margin-bottom: 2.5rem;
+  text-align: center;
+`;
+
+export const CTAWrapper = styled.div`
+  width: fit-content;
+`;
---
+++ renderer/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,40 @@
+import Button from "components/Buttons/Button";
+import Image from "next/image";
+import React from "react";
+import { Body1, Heading3 } from "styles/Typography";
+import * as S from "./EmptyState.style";
+
+interface EmptyStateProps {
+  title: string;
+  description: string;
+  image: string;
+  CTAText: string;
+  CTAIcon: string;
+}
+
+const EmptyState = (props: EmptyStateProps) => {
+  const { image, title, description, CTAText, CTAIcon } = props;
+  return (
+    <S.EmptyStateWrapper>
+      <S.EmptyStateImage>
+        <Image
+          src={`/images/${image}.png`}
+          alt="clients"
+          width={320}
+          height={320}
+        />
+      </S.EmptyStateImage>
+      <S.EmptyStateText>
+        <Heading3>{title}</Heading3>
+        <Body1>{description}</Body1>
+      </S.EmptyStateText>
+      <S.CTAWrapper>
+        <Button variant="primary" icon={CTAIcon}>
+          {CTAText}
+        </Button>
+      </S.CTAWrapper>
+    </S.EmptyStateWrapper>
+  );
+};
+
+export default EmptyState;
