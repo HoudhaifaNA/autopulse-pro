5184b58d5a0614cb4afe78358e89a06456923f47
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-28 09:54:58 +0100

    added login form with formik

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,30 +1,73 @@
-import Navbar from "components/Navbar/Navbar";
-import SearchBar from "components/Header/Header";
-import EmptyState from "components/EmptyState/EmptyState";
+import Image from "next/image";
+import { Formik, FormikProps, FormikHelpers } from "formik";
+import * as S from "styles/LoginPage.styled";
+import { Heading3 } from "styles/Typography";
+import { TypedInput } from "components/Input/Input";
+import Button from "components/Buttons/Button";
+import { useState } from "react";
+
+interface Values {
+  username: string;
+  password: string;
+}
+
+const INITIAL_VALUES = { username: "", password: "" };
+
+const onSubmit = (values: Values, actions: FormikHelpers<Values>) => {
+  setTimeout(() => {
+    console.log(values);
+    actions.resetForm();
+  }, 2000);
+};
 
 const LoginPage = () => {
+  const [visiblity, setVisibility] = useState(false);
+
   return (
-    <div style={{ padding: "0", display: "flex" }}>
-      <Navbar />
-      <div
-        style={{
-          flex: "1",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          background: "#F9F9F9",
-        }}
-      >
-        <SearchBar />
-        <EmptyState
-          title="Vous n'avez pas de clients"
-          description="Ajoutez des clients pour les voir ici"
-          image="clients"
-          CTAIcon="add"
-          CTAText="Ajouter un client"
-        />
-      </div>
-    </div>
+    <S.Home>
+      <S.Main>
+        <S.FormHeading>
+          <Image
+            src="/images/logo.png"
+            alt="zauto logo"
+            width={50}
+            height={50}
+          />
+          <Heading3>Se connecter</Heading3>
+        </S.FormHeading>
+        <Formik initialValues={INITIAL_VALUES} onSubmit={onSubmit}>
+          {({ handleSubmit, isSubmitting }: FormikProps<Values>) => {
+            return (
+              <S.LoginForm onSubmit={handleSubmit}>
+                <TypedInput
+                  name="username"
+                  type="text"
+                  label="Nom"
+                  placeholder="Entrez votre nom"
+                />
+                <TypedInput
+                  name="password"
+                  type={visiblity ? "text" : "password"}
+                  label="Mot de passe"
+                  placeholder="Entrez votre mot de pass"
+                  iconRight={visiblity ? "visibility_off" : "visibility"}
+                  onIconClick={() => setVisibility(!visiblity)}
+                />
+                <Button
+                  type="submit"
+                  variant="primary"
+                  width="100%"
+                  loading={isSubmitting}
+                  disabled={isSubmitting}
+                >
+                  Se connecter
+                </Button>
+              </S.LoginForm>
+            );
+          }}
+        </Formik>
+      </S.Main>
+    </S.Home>
   );
 };
 
