c6b804c71c9abd1bbd6fad0320adc83ecafbc19c
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-28 09:56:20 +0100

    added styling for the form

---
+++ renderer/styles/LoginPage.styled.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Home = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding-top: 14rem;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FormHeading = styled.div`
+  text-align: center;
+  margin-bottom: 3.5rem;
+`;
+
+export const LoginForm = styled.form`
+  width: 30rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
