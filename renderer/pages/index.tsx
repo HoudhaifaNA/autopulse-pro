6469cca94674345b4e381fd708a027204cb3d038
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-29 10:40:09 +0100

    added title page

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -3,16 +3,25 @@ import Image from "next/image";
 import * as S from "styles/LoginPage.styled";
 import { Heading3 } from "styles/Typography";
 import LoginForm from "components/LoginForm/LoginForm";
+import Meta from "components/Meta/Meta";
 
 const LoginPage = () => {
   return (
-    <S.Main>
-      <S.FormHeading>
-        <Image src="/images/logo.png" alt="zauto logo" width={50} height={50} />
-        <Heading3>Se connecter</Heading3>
-      </S.FormHeading>
-      <LoginForm />
-    </S.Main>
+    <>
+      <Meta title="Se connecter" />
+      <S.Main>
+        <S.FormHeading>
+          <Image
+            src="/images/logo.png"
+            alt="zauto logo"
+            width={50}
+            height={50}
+          />
+          <Heading3>Se connecter</Heading3>
+        </S.FormHeading>
+        <LoginForm />
+      </S.Main>
+    </>
   );
 };
 
