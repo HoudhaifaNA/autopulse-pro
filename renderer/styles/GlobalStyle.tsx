bbc083c999ae1b4fb23469806b41b89a096644ad
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-01-29 10:40:40 +0100

    added dark background for modal

--- renderer/styles/GlobalStyle.tsx
+++ renderer/styles/GlobalStyle.tsx
@@ -21,6 +21,16 @@ const GlobalStyle = createGlobalStyle`
 
     }
 
+
+    .background-black{
+        position: fixed;
+        left: 0;
+        top: 0;
+        z-index: 100;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba(0,0,0,.4);
+    }
   
 `;
 
