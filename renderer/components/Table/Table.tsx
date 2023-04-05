05b79ddbc83beee91ee7dce2311aba3688c6a4ea
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-04-05 22:40:07 +0100

    start refactoring table to table element

--- renderer/components/Table/Table.tsx
+++ renderer/components/Table/Table.tsx
@@ -1,67 +1,27 @@
-import { ReactNode, useState } from "react";
-import * as S from "components/Table/Table.styled";
-import { Body2 } from "styles/Typography";
-import Icon from "components/Icon/Icon";
-import Checkbox from "components/Checkbox/Checkbox";
+import styled from "styled-components";
 
-interface TableHeaderProps {
-  sortable?: boolean;
-  text: string;
-}
+export const Table = styled.table`
+  width: 100%;
+  margin-top: 5rem;
+  border-collapse: collapse;
+  overflow: hidden;
+`;
 
-export const TableHeader = ({ columns }: { columns: TableHeaderProps[] }) => {
-  return (
-    <S.TableHeader>
-      <S.TableCell>
-        <Checkbox />
-      </S.TableCell>
+export const TableRow = styled.tr<{ $header?: boolean }>`
+  height: 5rem;
+  background-color: ${({ $header, theme }) =>
+    $header ? theme.colors.primary[50] : "#fff"};
+  padding: 1.5rem 2rem;
+`;
 
-      {columns.map(({ text, sortable }) => {
-        return (
-          <S.TableCell key={text}>
-            <Body2>{text}</Body2>
-            {sortable && <Icon icon="expand" size="1.6rem" />}
-          </S.TableCell>
-        );
-      })}
-    </S.TableHeader>
-  );
-};
+export const TableCol = styled.td`
+  padding: 1rem;
+  div {
+    display: flex;
+    align-items: center;
+  }
 
-export const TableRow = ({ children }: { children: ReactNode[] }) => {
-  return (
-    <S.TableRow>
-      <S.TableCell>
-        <Checkbox />
-      </S.TableCell>
-      {children.map((el, index) => {
-        const [blur, setBlur] = useState(false);
-        return (
-          <S.TableCell $blur={blur} onClick={() => setBlur(!blur)} key={index}>
-            {el}
-          </S.TableCell>
-        );
-      })}
-      <S.TableCell style={{ position: "relative" }}>
-        <Icon icon="more_vert" size="1.6rem" />
-      </S.TableCell>
-    </S.TableRow>
-  );
-};
-
-const Table = ({
-  HeaderItems,
-  children,
-}: {
-  HeaderItems: TableHeaderProps[];
-  children: ReactNode;
-}) => {
-  return (
-    <S.TableWrapper>
-      <TableHeader columns={HeaderItems} />
-      {children}
-    </S.TableWrapper>
-  );
-};
-
-export default Table;
+  p {
+    white-space: nowrap;
+  }
+`;
