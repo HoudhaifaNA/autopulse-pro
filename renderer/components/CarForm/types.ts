f1e74270003fe966587d232abecb99bb33d615a5
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-04-14 17:25:06 +0100

    added car values interface

--- renderer/components/CarForm/types.ts
+++ renderer/components/CarForm/types.ts
@@ -1,3 +1,23 @@
+export interface Values {
+  step: number;
+  carType: "locale" | "importé";
+  brand: string;
+  serie: string;
+  model: string;
+  serialNumber: string;
+  registrationNumber: string;
+  color: string;
+  year: string;
+  seller: string;
+  boughtPrice: number;
+  euroPrice: number;
+  lisence: string;
+  depenses: {
+    type: "locale" | "À l'étranger";
+    raison: string;
+    cost: number;
+  }[];
+}
 export type setFieldValue = (
   field: string,
   value: any,
