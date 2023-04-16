33363b72548320b9fafa1f86ae149b36bf1de568
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-04-16 10:19:31 +0100

    added unoptimized images for build issues

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -2,11 +2,16 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = "electron-renderer";
     }
 
     return config;
+  },
+
+  images: {
+    unoptimized: true,
   },
 };
