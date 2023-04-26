4c6765491f871a9b3600d466acb692f25195c964
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-04-26 15:30:55 +0100

    refactored and moved css to another file

--- renderer/components/CarForm/PlaceForm.tsx
+++ renderer/components/CarForm/PlaceForm.tsx
@@ -1,44 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import * as S from "components/CarForm/PlaceForm.styled";
 
 import { setFieldValue } from "components/CarForm/types";
 import { Body1 } from "styles/Typography";
 import Icon from "components/Icon/Icon";
-
-const PlaceFormWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 7rem;
-  height: 20rem;
-`;
-export const PlaceItem = styled.div<{ $selected: boolean }>`
-  position: relative;
-  width: 19rem;
-  height: 12.5rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  text-transform: capitalize;
-  border: 0.2rem solid
-    ${({ $selected, theme }) =>
-      $selected ? theme.colors.primary[500] : theme.colors.neutral[300]};
-  border-radius: 0.4rem;
-  user-select: none;
-  cursor: pointer;
-`;
-
-const IconWrapper = styled.div`
-  position: absolute;
-  right: -1rem;
-  top: -1rem;
-  background-color: ${({ theme }) => theme.colors.white};
-  svg {
-    fill: ${({ theme }) => theme.colors.primary[500]};
-  }
-`;
 
 interface PlaceFormProps {
   carType: string;
@@ -47,27 +12,31 @@ interface PlaceFormProps {
 
 const places = ["importé", "locale"];
 
+const renderPlaces = (carType: string, setFieldValue: setFieldValue) => {
+  return places.map((place) => {
+    const isSelected = carType === place;
+
+    return (
+      <S.PlaceItem
+        key={place}
+        $selected={isSelected}
+        onClick={() => setFieldValue("carType", place)}
+      >
+        <S.IconWrapper>
+          {isSelected && <Icon icon="success" size="2.4rem" />}
+        </S.IconWrapper>
+        <Icon icon={place} size="4.8rem" />
+        <Body1>{place}</Body1>
+      </S.PlaceItem>
+    );
+  });
+};
+
 const PlaceForm = ({ carType, setFieldValue }: PlaceFormProps) => {
   return (
-    <PlaceFormWrapper>
-      {places.map((place) => {
-        const isSelected = carType === place;
-
-        return (
-          <PlaceItem
-            key={place}
-            $selected={isSelected}
-            onClick={() => setFieldValue("carType", place)}
-          >
-            <IconWrapper>
-              {isSelected && <Icon icon="success" size="2.4rem" />}
-            </IconWrapper>
-            <Icon icon={place} size="4.8rem" />
-            <Body1>{place}</Body1>
-          </PlaceItem>
-        );
-      })}
-    </PlaceFormWrapper>
+    <S.PlaceFormWrapper>
+      {renderPlaces(carType, setFieldValue)}
+    </S.PlaceFormWrapper>
   );
 };
 
