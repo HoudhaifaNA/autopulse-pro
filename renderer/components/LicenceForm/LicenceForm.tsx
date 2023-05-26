961943150dafcfc65e9e11b9d5726aa7f2be29cc
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-05-26 17:33:31 +0100

    Cleaned forms

--- renderer/components/LicenceForm/LicenceForm.tsx
+++ renderer/components/LicenceForm/LicenceForm.tsx
@@ -1,13 +1,13 @@
-import { Formik, FormikProps, FormikHelpers } from "formik";
+import { useState } from "react";
+import { FormikProps, FormikHelpers } from "formik";
 
-import { Form, FormContent, FormGroup } from "components/ui/Form.styled";
+import { FormGroup } from "components/Form/Form.styled";
 import * as S from "components/LicenceForm/LicenceForm.styled";
 
+import Form from "components/Form/Form";
 import { TypedInput, SelectInput } from "components/Input/Input";
 import Dropzone from "components/LicenceForm/Dropzone";
 import Preview from "components/LicenceForm/Preview";
-import Modal, { ModalActions, ModalContent } from "components/Modal/Modal";
-import Button from "components/Button/Button";
 
 import wilayas from "data/wilayas.json";
 import { licenceSchema } from "Schemas/FormSchemas";
@@ -42,78 +42,54 @@ const renderAttachments = (attachments: Values["attachments"]) => {
 };
 
 const LicenceForm = () => {
-  return (
-    <Modal title="Ajouter une licence">
-      <Formik
-        initialValues={INITIAL_VALUES}
-        validationSchema={licenceSchema}
-        onSubmit={onSubmit}
-      >
-        {(props: FormikProps<Values>) => {
-          const { handleSubmit, submitForm, isSubmitting, values } = props;
+  const [formProps, setFormProps] = useState<FormikProps<Values>>();
 
-          return (
-            <>
-              <ModalContent>
-                <Form onSubmit={handleSubmit}>
-                  <FormContent>
-                    <FormGroup>
-                      <SelectInput
-                        name="seller"
-                        label="Vendeur :"
-                        placeholder="Nom de vendeur"
-                        autoFocus
-                        items={[]}
-                      />
-                      <TypedInput
-                        name="moudjahid"
-                        type="text"
-                        label="Moudjahid :"
-                        placeholder="Nom du moudjahid"
-                      />
-                    </FormGroup>
-                    <FormGroup>
-                      <SelectInput
-                        name="wilaya"
-                        label="Wilaya :"
-                        placeholder="Entrez la wilaya"
-                        items={WILAYAS_ITEMS}
-                      />
-                      <TypedInput
-                        name="price"
-                        type="number"
-                        label="Prix :"
-                        placeholder="Prix ​​de la licence"
-                        addOn="DZD"
-                      />
-                    </FormGroup>
-                    <FormGroup>
-                      <Dropzone />
-                    </FormGroup>
-                    <S.DocumentsList>
-                      {renderAttachments(values.attachments)}
-                    </S.DocumentsList>
-                    {/*Add hidden input to submit button with hitting enter */}
-                    <input type="submit" style={{ display: "none" }} />
-                  </FormContent>
-                </Form>
-              </ModalContent>
-              <ModalActions>
-                <Button
-                  type="submit"
-                  variant="primary"
-                  loading={isSubmitting}
-                  disabled={isSubmitting}
-                  onClick={submitForm}
-                >
-                  Ajouter
-                </Button>
-              </ModalActions>
-            </>
-          );
-        }}
-      </Formik>
-    </Modal>
+  const values = formProps?.values;
+  const { attachments } = values ?? INITIAL_VALUES;
+
+  return (
+    <Form
+      title="Ajouter une licence"
+      initials={INITIAL_VALUES}
+      validation={licenceSchema}
+      onSubmit={onSubmit}
+      getFormProps={(props) => setFormProps(props)}
+    >
+      <FormGroup>
+        <SelectInput
+          name="seller"
+          label="Vendeur :"
+          placeholder="Nom de vendeur"
+          autoFocus
+          items={[]}
+        />
+        <TypedInput
+          name="moudjahid"
+          type="text"
+          label="Moudjahid :"
+          placeholder="Nom du moudjahid"
+        />
+      </FormGroup>
+      <FormGroup>
+        <SelectInput
+          name="wilaya"
+          label="Wilaya :"
+          placeholder="Entrez la wilaya"
+          items={WILAYAS_ITEMS}
+        />
+        <TypedInput
+          name="price"
+          type="number"
+          label="Prix :"
+          placeholder="Prix ​​de la licence"
+          addOn="DZD"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Dropzone />
+      </FormGroup>
+      <S.DocumentsList>{renderAttachments(attachments)}</S.DocumentsList>
+    </Form>
   );
 };
 
