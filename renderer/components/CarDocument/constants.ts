dff708337f82ae5f5cc7f14d7cd96f86ed5d0896
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-05-29 18:20:47 +0100

    Added car and client document viewer

---
+++ renderer/components/CarDocument/constants.ts
@@ -0,0 +1,63 @@
+export const CAR_DETAILS = [
+  {
+    columns: 3,
+    section: "Détails de la voiture",
+    details: {
+      voiture: `Mercedes-Benz CLA 200 4MATIC 8G-DCT`,
+      "numéro de châssis": "W1KZF8GB8NB093XXX",
+      matricule: "WG69 NXF",
+      couleur: "Noir",
+      année: "2022",
+    },
+  },
+  {
+    columns: 4,
+    section: "Détails d'achat",
+    details: {
+      vendeur: `Houdhaifa Lebbad`,
+      "prix ​​d'achat": "€150000.00",
+      "prix ​​de €100": "185",
+      "prix ​​final": "4809000.00 DZD",
+      licence: "Haroun Merouane",
+      "prix ​​de la licence": "17800.00 DZD _RD",
+    },
+  },
+  {
+    columns: 4,
+    section: "Dépenses",
+    details: {
+      "1": {
+        raison: "Bateau",
+        "coût ​​en €": "185.00 DZD",
+        "prix ​​de €100": "182.00 DZD",
+        "coût ​​final": "324500.00 DZD",
+      },
+      "2": { raison: "Bateau", prix: "185.00 DZD" },
+      "3": { raison: "Depanage", prix: "17685.00 DZD" },
+      "4": {
+        raison: "Bateau",
+        "coût ​​en €": "185.00 DZD",
+        "prix ​​de €100": "182.00 DZD",
+        "coût ​​final": "324500.00 DZD",
+      },
+    },
+  },
+
+  {
+    columns: 4,
+    section: "Détails de la vente",
+    details: {
+      acheteur: `Houdhaifa Lebbad`,
+      "prix ​​de vente": "150000.00 DZD",
+    },
+  },
+  {
+    columns: 4,
+    section: "Totaux",
+    details: {
+      "total des euros achetés": `€158605.00`,
+      "total du prix d'achat": "150000.00 DZD",
+      intérêt: "150000.00 DZD _GR",
+    },
+  },
+];
