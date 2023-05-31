1c2abd70543efe6540583abe7207bb7633bdc70e
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-05-31 09:58:38 +0100

    Added File type

--- renderer/components/LicenceForm/PDFPreview.tsx
+++ renderer/components/LicenceForm/PDFPreview.tsx
@@ -6,7 +6,7 @@ import * as S from "components/LicenceForm/PDFPreview.styled";
 // We have to setup a worker
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
-const PDFPreview = ({ file }: { file: string }) => {
+const PDFPreview = ({ file }: { file: string | File }) => {
   return (
     <S.PDFPreview>
       <Document file={file} loading={""}>
