a47f68c3cd24fb81eb227aca96b15f9fb6cfd187
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-06-04 14:21:54 +0100

    testing sql commands

--- server/app.ts
+++ server/app.ts
@@ -7,4 +7,19 @@ const app = express();
 
 app.use(cors());
 
+app.get("/api/cars", (_, res) => {
+  db.each(`SELECT * FROM cars`, [], function (err, rows) {
+    if (err) {
+      console.log(err.message);
+      return res.status(401).json({ message: "error", err: err.message });
+    }
+    return res.status(200).json({ message: "success ", rows });
+  });
+
+  db.close((err) => {
+    if (err) console.log(err.message);
+    console.log("Closed database successfully ✔");
+  });
+});
+
 export default app;
--- server/database.ts
+++ server/database.ts
@@ -2,11 +2,11 @@ import path from "path";
 import sqlite from "sqlite3";
 
 const isProd: boolean = process.env.NODE_ENV === "production";
-let dbPath = path.join(path.resolve(), "db", "db.db");
+let dbPath = path.join(path.resolve(), "../db", "db.db");
 if (isProd) dbPath = path.join(path.resolve(), "resources", "db/db.db");
 
 const db = new sqlite.Database(dbPath, sqlite.OPEN_READWRITE, (err) => {
-  if (err) return console.log("Database connection error 🔥", err);
+  if (err) return console.log("Database connection error 🔥", err.message);
   console.log("Connected to database successfully ✔");
 });
 
