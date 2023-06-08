de1643b0ff0d63025122b80df2384e02a136649d
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-06-08 15:57:57 +0100

    Changed db to better-sqlite3

--- server/database.ts
+++ server/database.ts
@@ -1,15 +1,13 @@
 import path from "path";
-import sqlite from "sqlite3";
+// import sqlite from "sqlite3";
+import Database from "better-sqlite3";
 
 const isProd: boolean = process.env.NODE_ENV === "production";
 let dbPath = path.join(path.resolve(), "db", "db.db");
 if (isProd) dbPath = path.join(path.resolve(), "resources", "db/db.db");
 
-const db = new sqlite.Database(dbPath, sqlite.OPEN_READWRITE, (err) => {
-  if (err) return console.log("Database connection error 🔥", err.message);
-  console.log("Connected to database successfully ✔");
-});
+const db = new Database(dbPath, { verbose: null });
 
-db.get("PRAGMA foreign_keys = ON");
+if (db.open) console.log("Connected successfully");
 
 export default db;
