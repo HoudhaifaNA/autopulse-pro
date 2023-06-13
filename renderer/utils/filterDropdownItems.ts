1a72282eeac2cc74042998fbb18cc191ead15482
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-06-13 18:24:09 +0100

    Added sorting to select dropdown

--- renderer/utils/filterDropdownItems.ts
+++ renderer/utils/filterDropdownItems.ts
@@ -1,9 +1,11 @@
 type TItems = { mainText: string }[];
 
 const filterDropdownItems = (items: TItems, query: string) => {
-  return items.filter(({ mainText }) => {
-    return mainText.toLowerCase().includes(query.toLowerCase());
-  });
+  return items
+    .filter(({ mainText }) => {
+      return mainText.toLowerCase().includes(query.toLowerCase());
+    })
+    .sort((prev, curr) => prev.mainText.localeCompare(curr.mainText));
 };
 
 export default filterDropdownItems;
