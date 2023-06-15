b43f10389d2a06a0ba00539c9a6515199de73be0
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-06-15 07:26:14 +0100

    Added empty state CTA

--- renderer/components/EmptyState/EmptyState.tsx
+++ renderer/components/EmptyState/EmptyState.tsx
@@ -1,9 +1,11 @@
+import { useContext } from "react";
 import Image from "next/image";
 
 import * as S from "components/EmptyState/EmptyState.styled";
 import { Body1, Heading3 } from "styles/Typography";
 
 import Button from "components/Button/Button";
+import { GlobalContext } from "pages/_app";
 
 interface EmptyStateProps {
   title: string;
@@ -11,12 +13,23 @@ interface EmptyStateProps {
   image: string;
   CTAText: string;
   CTAIcon: string;
+  modal: string;
+  handleClick?: () => void;
   IconP?: "right" | "left";
 }
 
 const EmptyState = (props: EmptyStateProps) => {
-  const { image, title, description, CTAText, CTAIcon, IconP } = props;
-
+  const {
+    image,
+    title,
+    description,
+    CTAText,
+    CTAIcon,
+    modal,
+    handleClick,
+    IconP,
+  } = props;
+  const { _, setModal } = useContext(GlobalContext);
   return (
     <S.EmptyStateWrapper>
       <S.EmptyStateImage>
@@ -31,7 +44,15 @@ const EmptyState = (props: EmptyStateProps) => {
         <Heading3>{title}</Heading3>
         <Body1>{description}</Body1>
       </S.EmptyStateText>
-      <Button variant="primary" icon={CTAIcon} iconPostition={IconP}>
+      <Button
+        variant="primary"
+        icon={CTAIcon}
+        iconPostition={IconP}
+        onClick={() => {
+          modal && setModal(modal);
+          handleClick && handleClick();
+        }}
+      >
         {CTAText}
       </Button>
     </S.EmptyStateWrapper>
