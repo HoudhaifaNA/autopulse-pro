b1ecc4a796abf54430b9dbd6b656835bf1dc06da
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-06-19 07:29:49 +0100

    Changed select statment

--- server/controllers/searchController.ts
+++ server/controllers/searchController.ts
@@ -1,33 +1,14 @@
 import db from "../database";
+import { CAR_SELECT_STMT } from "../statments/carStatments";
 import { SELECT_BASE_QUERY } from "../statments/licenceStatments";
 import tryCatch from "../utils/tryCatch";
 
 const serachClients = db.prepare(`SELECT * FROM clients 
  WHERE fullName LIKE ? LIMIT 3
 `);
-`SELECT cars.*, 
-  moudjahid,
-  licences.price AS licencePrice,
-  clients.fullName AS seller,
-  CASE WHEN cars.buyerId IS NULL THEN NULL ELSE buyers.fullName END AS buyer,
-  datetime(cars.created_at,'localtime') AS created_at, 
-  datetime(cars.updated_at,'localtime') AS updated_at
-  FROM cars
-  INNER JOIN licences ON licences.id = licenceId
-  INNER JOIN clients ON clients.id = cars.sellerId
-  LEFT JOIN clients AS buyers ON buyers.id = cars.buyerId`;
+
 const serachCars = db.prepare(`
-SELECT cars.*, 
-  moudjahid,
-  licences.price AS licencePrice,
-  clients.fullName AS seller,
-  CASE WHEN cars.buyerId IS NULL THEN NULL ELSE buyers.fullName END AS buyer,
-  datetime(cars.created_at,'localtime') AS created_at, 
-  datetime(cars.updated_at,'localtime') AS updated_at
-  FROM cars
-  INNER JOIN licences ON licences.id = licenceId
-  INNER JOIN clients ON clients.id = cars.sellerId
-  LEFT JOIN clients AS buyers ON buyers.id = cars.buyerId
+  ${CAR_SELECT_STMT}
  WHERE cars.name LIKE ? OR cars.serialNumber LIKE ? LIMIT 3
 `);
 
