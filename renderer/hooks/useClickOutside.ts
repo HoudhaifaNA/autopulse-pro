bfd27fbbb9c5b09549f239bd99f8d8473884035d
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-09-27 08:59:35 +0100

    Fixed useClickOutside hook

--- renderer/hooks/useClickOutside.ts
+++ renderer/hooks/useClickOutside.ts
@@ -1,20 +1,36 @@
-import { useEffect, useState, RefObject } from "react";
+import { useEffect, useState } from "react";
 
-const useClickOutside = (ref: RefObject<HTMLElement>) => {
-  const [isFocused, setFocus] = useState(false);
-
-  const onClickOutside = (e: MouseEvent) => {
-    const { current } = ref;
-    const target = e.target as Node | null;
-    if (current && !current.contains(target)) setFocus(false);
-  };
+const useClickOutside = (targetId: string, togglerId: string, defaultState: boolean = true) => {
+  const [isOutside, setIsOutside] = useState(defaultState);
 
   useEffect(() => {
-    document.addEventListener("click", onClickOutside);
-    return () => document.removeEventListener("click", onClickOutside);
-  }, []);
+    const handleClickOutside = (event: MouseEvent) => {
+      const targetElement = document.getElementById(targetId);
+      const togglerElement = document.getElementById(togglerId);
 
-  return [isFocused, setFocus] as const;
+      const isChild = targetElement?.contains(event.target as Node);
+      const isTogglerElement = event.target === togglerElement || togglerElement?.contains(event.target as Node);
+      const isMantineBtn = (event.target as Element).classList?.contains("mantine-UnstyledButton-root");
+
+      if (!isChild && !isTogglerElement && !isMantineBtn) {
+        setIsOutside(true);
+      } else if (isChild) {
+        setIsOutside(false);
+      }
+    };
+
+    const handleDocumentClick = (event: MouseEvent) => {
+      handleClickOutside(event);
+    };
+
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [targetId, togglerId]);
+
+  return [isOutside, setIsOutside] as const;
 };
 
 export default useClickOutside;
