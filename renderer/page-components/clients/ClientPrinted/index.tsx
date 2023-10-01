23bf16c2ca2aaa34f4272a93f9c3ae6a55862cff
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-10-01 18:20:38 +0100

    Finished client printed page for beta testing

--- renderer/page-components/clients/ClientPrinted/index.tsx
+++ renderer/page-components/clients/ClientPrinted/index.tsx
@@ -1,20 +1,101 @@
-import { Ref, forwardRef, useEffect } from "react";
+import Image from "next/image";
+import { Ref, forwardRef } from "react";
+
 import * as S from "./styles";
+import { Body2, Heading3, LabelText } from "styles/Typography";
+import FiatItemLine from "./FiatItemLine";
+import TransactionsTable from "../TransactionsTable";
 
 import useClientTransactions from "hooks/useClientTransactions";
-import TransactionsTable from "../TransactionsTable";
+import useClientLastTransaction from "hooks/useClientLastTransaction";
+import formatDate from "utils/formatDate";
 
 interface ClientPrintedProps {
   id: string | number;
+  type: "last" | "all";
 }
 
-const ClientPrinted = forwardRef(({ id }: ClientPrintedProps, ref: Ref<HTMLDivElement>) => {
-  const { data } = useClientTransactions(id);
+const ClientPrinted = forwardRef(({ id, type }: ClientPrintedProps, ref: Ref<HTMLDivElement>) => {
+  const { data } = type === "all" ? useClientTransactions(id) : useClientLastTransaction(id);
+
+  const renderTransactionsTable = () => {
+    if (data && "transactions" in data) {
+      const {
+        eur_balance,
+        dzd_balance,
+        total_entrante_eur,
+        total_sortante_eur,
+        total_sortante_dzd,
+        total_entrante_dzd,
+      } = data.client;
+
+      return (
+        <>
+          <TransactionsTable transactions={data.transactions} showSymbol />
+          <FiatItemLine label="Total Sorties (EUR)" value={total_sortante_eur} currency="EUR" />
+          <FiatItemLine label="Total Entrées (EUR)" value={total_entrante_eur} currency="EUR" />
+          <FiatItemLine label="Solde actuel (EUR)" value={eur_balance} currency="EUR" />
+          <FiatItemLine label="Total Sorties (DZD)" value={total_sortante_dzd} currency="DZD" />
+          <FiatItemLine label="Total Entrées (DZD)" value={total_entrante_dzd} currency="DZD" />
+          <FiatItemLine label="Solde actuel (DZD)" value={dzd_balance} currency="DZD" />
+        </>
+      );
+    }
+  };
+
+  const renderLastTransaction = () => {
+    if (data && "last_transaction" in data) {
+      const { dzd_balance, eur_balance } = data.client;
+      const { currency, amount } = data.last_transaction;
+
+      let currentBalance = currency === "EUR" ? eur_balance : dzd_balance;
+      const lastBalance = currentBalance - amount;
+
+      return (
+        <>
+          <FiatItemLine label={`Dernier solde (${currency})`} value={lastBalance} currency={currency} />
+          <TransactionsTable transactions={[data.last_transaction]} showSymbol />
+          <FiatItemLine label={`Solde actuel (${currency})`} value={currentBalance} currency={currency} />
+        </>
+      );
+    }
+  };
 
   return (
     <S.DocumentWrapper ref={ref} id="content">
-      {data && <TransactionsTable transactions={data.transactions} />}
-      <div className="page-number"></div>
+      <S.DocumentHeader>
+        <S.DocumentLogo>
+          <Image src="/images/logo.png" alt="zauto logo" width={200} height={70} />
+        </S.DocumentLogo>
+        <S.CompanyDetails>
+          <LabelText as="p">zauto.nadhir@gmail.com</LabelText>
+          <LabelText as="p">0674987298 - 0550733738 - 0550667971</LabelText>
+          <LabelText as="p">Smara , El Eulma, Sétif (19123)</LabelText>
+        </S.CompanyDetails>
+      </S.DocumentHeader>
+      <S.Breaker>
+        <S.BreakerLine />
+        <Heading3>FACTURE</Heading3>
+        <S.BreakerLine />
+      </S.Breaker>
+      {data && (
+        <>
+          <S.DetailLine $type="stacked">
+            <LabelText as="p">Date d'émission :</LabelText>
+            <Body2>{formatDate(new Date().toISOString())}</Body2>
+          </S.DetailLine>
+          <S.DetailLine $type="stacked">
+            <LabelText as="p">Nom :</LabelText>
+            <Body2>{data.client.full_name}</Body2>
+          </S.DetailLine>
+          {type === "all" && renderTransactionsTable()}
+          {type === "last" && renderLastTransaction()}
+        </>
+      )}
+      <S.DocumentFooter>
+        <LabelText as="p">Signature du destinataire</LabelText>
+        <LabelText as="p">Signature du ZAUTO</LabelText>
+      </S.DocumentFooter>
     </S.DocumentWrapper>
   );
 });
--- renderer/page-components/clients/ClientPrinted/styles.ts
+++ renderer/page-components/clients/ClientPrinted/styles.ts
@@ -1,9 +1,18 @@
-import { TableHeaderCell } from "components/Table/styles";
-import { TableRow } from "components/Table/styles";
-import { TableHead, TableWrapper } from "components/Table/styles";
 import styled from "styled-components";
 
+import { TableWrapper, TableHead, TableRow } from "components/Table/styles";
+
+interface DetailLineProps {
+  $type: "stacked" | "spaced";
+}
+
 export const DocumentWrapper = styled.div`
+  position: relative;
+  min-height: 27.7cm;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+
   @page {
     size: A4;
     margin: 5mm;
@@ -11,33 +20,27 @@ export const DocumentWrapper = styled.div`
   }
 
   ${TableWrapper} {
+    min-height: max-content;
     max-height: max-content;
     overflow: visible;
   }
 
   ${TableHead} {
-    /* position: relative; */
     display: none;
   }
 
   ${TableRow}:first-child {
-    border-top: 0.2rem solid;
-    border-bottom: 0.2rem solid;
-    border-right: 0.2rem solid;
-    border-left: 0.2rem solid;
-    border-color: ${({ theme }) => theme.colors.neutral[200]};
+    border: 0.2rem solid ${({ theme }) => theme.colors.neutral[900]};
   }
+
+  ${TableRow} {
+    border-color: ${({ theme }) => theme.colors.neutral[900]};
+  }
+
   ${TableRow} {
     min-height: 6rem;
     page-break-inside: avoid;
-  }
-
-  .page-number {
-    position: absolute;
-    top: 1rem; /* Adjust the top position as needed */
-    right: 1rem; /* Adjust the right position as needed */
-    font-size: 1.4rem;
-    content: "Page " counter(page); /* Use the counter() function to display the page number */
+    page-break-before: avoid;
   }
 
   a {
@@ -48,5 +51,59 @@ export const DocumentWrapper = styled.div`
 
   .blurred {
     filter: blur(0px);
+  }
+`;
+
+export const DocumentHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DocumentLogo = styled.div``;
+
+export const CompanyDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  text-align: right;
+`;
+
+export const Breaker = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const BreakerLine = styled.div`
+  height: 2rem;
+  flex: 1;
+  background-color: #bc0707;
+
+  &:first-child {
+    flex-basis: 60%;
+  }
+`;
+
+export const DetailLine = styled.div<DetailLineProps>`
+  display: flex;
+  align-items: center;
+  justify-content: ${({ $type }) => ($type === "spaced" ? "space-between" : "flex-start")};
+  gap: ${({ $type }) => ($type === "stacked" ? "1rem" : "0")};
+
+  p:first-child {
+    font-weight: 700;
+  }
+`;
+
+export const DocumentFooter = styled.div`
+  margin-top: auto;
+  width: 100%;
+  height: 7rem;
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    font-weight: 700;
   }
 `;
