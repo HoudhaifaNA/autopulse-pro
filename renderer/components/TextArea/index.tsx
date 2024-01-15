bc51592c2266d3031afe75307daa06cc465111e8
Houdhaifa <Houdhaifadeveloper@gmail.com>
2024-01-15 10:04:17 +0100

    Added submit function to text area

--- renderer/components/TextArea/index.tsx
+++ renderer/components/TextArea/index.tsx
@@ -1,4 +1,5 @@
-import { FieldHookConfig, useField } from "formik";
+import { KeyboardEvent } from "react";
+import { FieldHookConfig, useField, useFormikContext } from "formik";
 
 import * as S from "./styles";
 import { LabelText } from "styles/Typography";
@@ -10,12 +11,17 @@ interface TextAreaAdditionalProps {
 type TextAreaProps = TextAreaAdditionalProps & FieldHookConfig<any>;
 
 const TextArea = ({ name, label }: TextAreaProps) => {
+  const { submitForm } = useFormikContext();
   const [props] = useField(name);
+
+  const handleSubmit = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.ctrlKey && e.code == "Enter") submitForm();
+  };
 
   return (
     <S.TextAreaWrapper>
       <LabelText>{label}</LabelText>
-      <S.TextAreaInput {...props} />
+      <S.TextAreaInput {...props} onKeyDown={handleSubmit} />
     </S.TextAreaWrapper>
   );
 };
