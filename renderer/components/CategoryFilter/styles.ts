b64dd687d8938b79584bd4f26d011ff0bfe0194d
Houdhaifa <Houdhaifadeveloper@gmail.com>
2024-01-15 09:25:21 +0100

    Changed style from flexbox to grid

--- renderer/components/CategoryFilter/styles.ts
+++ renderer/components/CategoryFilter/styles.ts
@@ -10,28 +10,26 @@ export const CategoryFilterWrapper = styled.div`
   gap: 1rem;
 `;
 export const CategoryFilterList = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 4rem;
-  /* border: 0.2rem solid ${({ theme }) => theme.colors.primary[500]}; */
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  min-height: 4rem;
+  gap: 0.2rem;
+  max-width: 100%;
 `;
 
 export const CategoryFilterOption = styled.div<CategoryFilterOptionProps>`
+  grid-column: span 1;
   display: flex;
   align-items: center;
   justify-content: center;
   text-align: center;
   flex: 1;
-  height: 100%;
+  min-height: 4rem;
+  padding: 0 0.5rem;
   color: ${({ theme, $isSelected }) => ($isSelected ? theme.colors.white : theme.colors.primary[500])};
   background-color: ${({ theme, $isSelected }) => ($isSelected ? theme.colors.primary[500] : theme.colors.white)};
-  border: 0.2rem solid ${({ theme }) => theme.colors.primary[500]};
-  border-right: none;
+  border: 0.2rem solid ${({ theme }) => theme.colors.primary[300]};
+  border-radius: 0.4rem;
   transition: all 0.2s ease-in;
   cursor: pointer;
-
-  &:last-child {
-    border-right: 0.2rem solid ${({ theme }) => theme.colors.primary[500]};
-  }
 `;
